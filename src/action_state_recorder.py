dd1f131e19ef19587c09b1b3d4a0c5775b77f72c
ZhiangChen <zxc251@case.edu>
2017-10-20 10:14:01 -0700

    data

--- src/action_state_recorder.py
+++ src/action_state_recorder.py
@@ -9,6 +9,7 @@ import rospy
 from geometry_msgs.msg import PoseArray as PA
 from geometry_msgs.msg import Vector3
 import numpy as np
+import pickle
 
 class ASRecorder(object):
     """
@@ -19,7 +20,6 @@ class ASRecorder(object):
         self.sub2 = rospy.Subscriber('action', Vector3, self.callback2, queue_size=1)
         self.action_state = list()
         self.crt_state = list()
-        self.f = open("action_state_data", "w")
 
     def callback1(self, pa):
         n_px = [pa.poses[i].position.x for i in range(5)]
@@ -32,16 +32,15 @@ class ASRecorder(object):
         self.crt_state = np.array([n_px,n_py,n_pz,n_ox,n_oy,n_oz,n_ow]).transpose()
 
     def callback2(self, v3):
-        if self.crt_state.len() != 0:
+        if self.crt_state.shape[0] != 0:
             crt_action = np.array([v3.x, v3.y, v3.z])
             crt_action_state = {"action":crt_action,"state":self.crt_state}
             print '*'*40
             print crt_action_state
-            self.f.write(crt_action_state)
+            self.action_state.append(crt_action_state)
+            with open("action_state_data",'wb') as wfp:
+                pickle.dump(self.action_state, wfp)
 
-    def close_file(self):
-        self.f.close()
-        print "closed file"
 
 
 if __name__=='__main__':
--- src/run_action.py
+++ src/run_action.py
@@ -9,6 +9,7 @@ import sys
 import rospy
 from soft_arm.srv import *
 from geometry_msgs.msg import Vector3
+import numpy as np
 
 control = Vector3()
 control.x = 20
@@ -16,7 +17,7 @@ control.y = 20
 control.z = 0
 
 def run_action(control):
-	rospy.wait_for_service('airpress_control')
+	rospy.wait_for_service('airpress_control',timeout=5)
 	try:
 		client = rospy.ServiceProxy('airpress_control', OneSeg)
 		resp = client(control)
@@ -33,16 +34,32 @@ if __name__ == "__main__":
 		control.z = float(sys.argv[3])
 		print run_action(control)
 
-	elif sys.argv[1]=='all':
-		for x in range(60):
-			for y in range(60):
-				for z in range(60):
-					control.x = x
-					control.y = y
-					control.z = z
-					print run_action(control)
-					pub.publish(control)
+	elif len(sys.argv)==2:
+		if sys.argv[1]=='all':
+			for x in range(0,45,5):
+				for y in range(0,45,5):
+					for z in range(0,45,5):
+						control.x = x
+						control.y = y
+						control.z = z
+						print run_action(control)
+						rospy.sleep(4.0)
+						print "done"
+						pub.publish(control)
 
+	elif len(sys.argv) == 3:
+		if (sys.argv[1] == 'random'):
+			for _ in range(int(sys.argv[2])):
+				x,y,z = np.random.randint(0,41,size=3)
+				print x, y, z
+				control.x = x
+				control.y = y
+				control.z = z
+				print run_action(control)
+				rospy.sleep(5.0)
+				print "done"
+				pub.publish(control)
 
 	else:
+		print "run default"
 		print run_action(control)
